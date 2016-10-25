5801c3cb2018fe1789b1c7c042aad9c60df19a0b
axle2005 <arnoldryan2005@live.ca>
2016-10-25 11:36:13 -0300

    Added Reload Command

---
+++ src/main/java/io/github/axle2005/clearmob/commands/CommandReload.java
@@ -0,0 +1,34 @@
+package io.github.axle2005.clearmob.commands;
+
+import org.spongepowered.api.command.CommandException;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.text.Text;
+
+import io.github.axle2005.clearmob.ClearMob;
+
+public class CommandReload implements CommandExecutor{
+
+	ClearMob plugin;
+	
+	public CommandReload(ClearMob plugin)
+	{
+		this.plugin = plugin;
+	}
+	@Override
+	public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
+		
+		plugin.reload();
+		plugin.getLogger().info("Reloaded Successfully");
+		if(src instanceof Player)
+		{
+			src.sendMessage(Text.of("Reload complete"));
+		}
+		return CommandResult.success();
+	}
+
+	
+}

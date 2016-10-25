69cdaa8c6a3c57e2bf23acfd59af7221dcb8d185
axle2005 <arnoldryan2005@live.ca>
2016-10-25 11:33:41 -0300

    Moved command registering to its own class.

---
+++ src/main/java/io/github/axle2005/clearmob/commands/Register.java
@@ -0,0 +1,41 @@
+package io.github.axle2005.clearmob.commands;
+
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.spec.CommandSpec;
+import org.spongepowered.api.text.Text;
+
+import io.github.axle2005.clearmob.ClearMob;
+
+public class Register {
+	
+	
+	
+	public Register(ClearMob plugin)
+	{
+		CommandSpec run = CommandSpec.builder().permission("clearmob.run").description(Text.of("Clear entities"))
+				.executor(new CommandRun(plugin)).build();
+		
+		CommandSpec reload = CommandSpec.builder().permission("clearmob.reload").description(Text.of("Clear entities"))
+				.executor(new CommandReload(plugin)).build();
+
+		//CommandSpec add = CommandSpec.builder().permission("clearmob.add").description(Text.of("Add's Entity to List"))
+		//		.executor(new CommandAdd(plugin, config)).build();
+
+		CommandSpec dump = CommandSpec.builder().permission("clearmob.dump")
+				.description(Text.of("Dump's World Entities to Console/Logs"))
+				// .arguments(GenericArguments.string(Text.of("tileentity/entity")))
+				.executor(new CommandDump(plugin)).build();
+		
+		CommandSpec stats = CommandSpec.builder().permission("clearmob.tps")
+				.description(Text.of("Provides current stats of server"))
+				.executor(new CommandStats()).build();
+
+		CommandSpec clearmob = CommandSpec.builder().description(Text.of("ClearMob Command")).child(run, "run")
+				.child(dump, "dump").child(stats, "tps").child(reload, "reload")
+				// .child(add, "add")
+
+				.build();
+
+		Sponge.getCommandManager().register(plugin, clearmob, "ClearMob");
+	}
+}

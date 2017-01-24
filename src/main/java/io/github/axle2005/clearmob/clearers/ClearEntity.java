3dffc95641752f4a4af9470b5b8c3f6737ab339e
axle2005 <arnoldryan2005@live.ca>
2017-01-23 22:21:17 -0400

    Push check to prevent Items/Players from being removed by command.

--- src/main/java/io/github/axle2005/clearmob/clearers/ClearEntity.java
+++ src/main/java/io/github/axle2005/clearmob/clearers/ClearEntity.java
@@ -8,6 +8,7 @@ import org.spongepowered.api.Sponge;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.entity.Entity;
 import org.spongepowered.api.entity.EntityType;
+import org.spongepowered.api.entity.Item;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.cause.Cause;
 import org.spongepowered.api.text.Text;
@@ -28,7 +29,7 @@ public class ClearEntity {
 			}
 
 		}
-
+		
 		if (!e.isEmpty()) {
 			for (Entity entity : e) {
 				for (int i = 0; i <= list.size() - 1; i++) {
@@ -59,6 +60,7 @@ public class ClearEntity {
 
 		if (!e.isEmpty()) {
 			for (Entity entity : e) {
+				if(!(entity instanceof Item) && !(entity instanceof Player))
 				if ((entity.getType().equals(tile))) {
 					entity.getLocation()
 							.removeBlock(Cause.source(Sponge.getPluginManager().fromInstance(plugin).get()).build());

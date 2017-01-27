cfb311a02298e25ec9d6f33d94aeaec62f07593d
axle2005 <arnoldryan2005@live.ca>
2017-01-27 19:03:40 -0400

    NPE was actually caused by wrong arguement structure.

--- src/main/java/io/github/axle2005/clearmob/commands/Register.java
+++ src/main/java/io/github/axle2005/clearmob/commands/Register.java
@@ -7,39 +7,35 @@ import org.spongepowered.api.text.Text;
 
 import io.github.axle2005.clearmob.ClearMob;
 
-
-
 public class Register {
-	
-	
-	
-	public Register(ClearMob plugin)
-	{
+
+	public Register(ClearMob plugin) {
 		CommandSpec run = CommandSpec.builder().permission("clearmob.run").description(Text.of("Clear entities"))
-				.arguments(GenericArguments.onlyOne(GenericArguments.string(Text.of("tileentity|entity|items"))))
-				.arguments(GenericArguments.optional(GenericArguments.string(Text.of("name"))))
+				.arguments(GenericArguments.onlyOne(GenericArguments.string(Text.of("tileentity|entity|items"))),
+						GenericArguments.optional(GenericArguments.string(Text.of("name"))))
 				.executor(new CommandRun(plugin)).build();
-		
+
 		CommandSpec reload = CommandSpec.builder().permission("clearmob.reload").description(Text.of("Clear entities"))
 				.executor(new CommandReload(plugin)).build();
 
-		//CommandSpec add = CommandSpec.builder().permission("clearmob.add").description(Text.of("Add's Entity to List"))
-		//		.executor(new CommandAdd(plugin, config)).build();
+		// CommandSpec add =
+		// CommandSpec.builder().permission("clearmob.add").description(Text.of("Add's
+		// Entity to List"))
+		// .executor(new CommandAdd(plugin, config)).build();
 
 		CommandSpec dump = CommandSpec.builder().permission("clearmob.dump")
 				.description(Text.of("Dump's World Entities to Console/Logs"))
 				.arguments(GenericArguments.onlyOne(GenericArguments.string(Text.of("tileentity|entity|nearby|all"))))
 				.executor(new CommandDump(plugin)).build();
-		
+
 		CommandSpec info = CommandSpec.builder().permission("clearmob.info")
 				.arguments(GenericArguments.onlyOne(GenericArguments.string(Text.of("range/hand"))),
 						GenericArguments.optional(GenericArguments.integer(Text.of("range"))))
-				.description(Text.of("Gets the Item Id of the item in hand. "))
-				.executor(new CommandInfo(plugin)).build();
-		
+				.description(Text.of("Gets the Item Id of the item in hand. ")).executor(new CommandInfo(plugin))
+				.build();
+
 		CommandSpec stats = CommandSpec.builder().permission("clearmob.tps")
-				.description(Text.of("Provides current stats of server"))
-				.executor(new CommandStats()).build();
+				.description(Text.of("Provides current stats of server")).executor(new CommandStats()).build();
 
 		CommandSpec clearmob = CommandSpec.builder().description(Text.of("ClearMob Command")).child(run, "run")
 				.child(dump, "dump").child(stats, "tps").child(reload, "reload").child(info, "info")
@@ -49,5 +45,5 @@ public class Register {
 
 		Sponge.getCommandManager().register(plugin, clearmob, "ClearMob");
 	}
-	
+
 }

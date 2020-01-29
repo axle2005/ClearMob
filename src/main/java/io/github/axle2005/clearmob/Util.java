f7e3d9d2e9a787df83c2bc681922c72343b3c7d1
axle2005 <arnoldryan2005@live.ca>
2020-01-29 16:26:53 -0400

    Update Util for Overhaul

--- src/main/java/io/github/axle2005/clearmob/Util.java
+++ src/main/java/io/github/axle2005/clearmob/Util.java
@@ -20,15 +20,10 @@ import org.spongepowered.api.Sponge;
 import org.spongepowered.api.block.tileentity.TileEntityType;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.entity.EntityType;
-import org.spongepowered.api.entity.EntityTypes;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.item.ItemType;
 import org.spongepowered.api.scheduler.Task.Builder;
 import org.spongepowered.api.text.Text;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class Util {
 
@@ -42,55 +37,6 @@ public class Util {
         }
     }
 
-    public static List<EntityType> getEntityType(List<String> entitys) {
-        List<EntityType> listEntityType = new ArrayList<>(Arrays.asList(EntityTypes.PRIMED_TNT));
-        for (String s : entitys) {
-            if (Sponge.getRegistry().getType(EntityType.class, s).isPresent()) {
-                listEntityType.add(Sponge.getRegistry().getType(EntityType.class, s).get());
-            } else if (s.contains("*")) {
-                listEntityType.addAll(Sponge.getRegistry().getAllFor(s.split(":")[0], EntityType.class));
-            }
-        }
-
-        return listEntityType;
-    }
-
-    public static List<TileEntityType> getTileEntityType(List<String> entitys) {
-        List<TileEntityType> listEntityType = new ArrayList<TileEntityType>();
-        for (String s : entitys) {
-            if (Sponge.getRegistry().getType(TileEntityType.class, s).isPresent()) {
-                listEntityType.add(Sponge.getRegistry().getType(TileEntityType.class, s).get());
-            } else if (s.contains("*")) {
-                listEntityType.addAll(Sponge.getRegistry().getAllFor(s.split(":")[0], TileEntityType.class));
-            }
-        }
-
-        return listEntityType;
-    }
-
-    public static List<ItemType> getItemType(List<String> entitys) {
-        List<ItemType> listEntityType = new ArrayList<ItemType>();
-        for (String s : entitys) {
-
-            //I don't even remember why I did this... variants maybe? modded blocks?
-            if (s.contains(":")) {
-                String[] y = s.split(":");
-                if (y.length == 3 || y[1].equals("*")) {
-                    s = (y[0] + ":" + y[1]);
-                }
-            }
-            if (Sponge.getRegistry().getType(ItemType.class, s).isPresent()) {
-                listEntityType.add(Sponge.getRegistry().getType(ItemType.class, s).get());
-            } else if (s.contains("*")) {
-                listEntityType.addAll(Sponge.getRegistry().getAllFor(s.split(":")[0], ItemType.class));
-            }
-
-
-        }
-
-        return listEntityType;
-    }
-
     public static EntityType getEntityType(String s) {
         return Sponge.getRegistry().getType(EntityType.class, s).get();
 
@@ -98,6 +44,11 @@ public class Util {
 
     public static TileEntityType getTileEntityType(String s) {
         return Sponge.getRegistry().getType(TileEntityType.class, s).get();
+
+    }
+
+    public static ItemType getItemType(String s) {
+        return Sponge.getRegistry().getType(ItemType.class, s).get();
 
     }
 
